3a2fe23503a6d0f911aa703056a06bb6ac7658b3
ZINEB TALGHMET <zinebtalghmet2022@gmail.com>
2026-05-21 22:29:40 +0100

    CAN-10: Auth & Authorization - Policies, AuthorizesRequests trait, protected resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\CandidatureController;
+use App\Http\Controllers\EntretienController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -15,6 +16,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('candidatures',CandidatureController::class);
+    Route::resource('entretiens',EntretienController::class);
 });
 
 require __DIR__.'/auth.php';
